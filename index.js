5a3d0f9567b5ab456cdd0c96fa2302debc7231ec
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-03-26 17:50:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "jean",
+    streetAddress: "#1, delmas",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]:value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const copyEmployee = { ...employee }; 
+    delete copyEmployee[key]; 
+    return copyEmployee; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
